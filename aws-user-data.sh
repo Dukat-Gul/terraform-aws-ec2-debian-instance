1e158697639b41299c30774e75cdccf2373548f2
Gul Dukat <81039430+Dukat-Gul@users.noreply.github.com>
2023-06-21 11:03:57 +0800

    Update aws-user-data.sh
    Let’s Encrypt apt packages

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 certbot python3-certbot-apache
 sudo systemctl start apache2
 sudo systemctl enable apache2
 echo "<h1>Test AWS</h1>" | sudo tee /var/www/html/index.html
